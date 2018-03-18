59f21dc7f94bb5f508638040d25a0a3c5a97600b
Diego Leite <diegojleite@gmail.com>
2018-03-18 13:36:07 -0300

    Express server working and showling latest 50 inserted posts :D

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI);
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    const Post = require('./models/post');
+
+    Post.find({}).limit(50).exec(function(error, data) {
+        res.render('pages/index', {posts: data});
+    });
 });
 
 app.listen(8080);
