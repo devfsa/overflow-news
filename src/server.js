13fe71909284f2d33dd2f7c195a6d6077ae6d3d6
Diego Leite <diegojleite@gmail.com>
2018-03-19 14:59:59 -0300

    Bring screenshot back

--- src/server.js
+++ src/server.js
@@ -12,14 +12,14 @@ app.set('view engine', 'ejs');
 app.get('/', async (req, res) => {
     const posts = await Post.find({})
         .limit(30)
-        .sort({'date': -1})
+        .sort({'date': -1});
     res.render('index', { posts });
 });
 
 app.get('/feeds', async (req, res) => {
     const feeds = await Feed.find({})
         .limit(50)
-        .sort({'date': -1})
+        .sort({'date': -1});
     res.render('feeds', { feeds });
 });
 
