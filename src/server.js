bd6ee45f2b335171d0052038a7b2164c72426e2b
Diego Leite <diegojleite@gmail.com>
2018-03-21 22:39:59 -0300

    Rollback frontend style

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import './bootstrap'
 import express from 'express'
-import twig from 'twig'
 import path from 'path'
 import dotenv from 'dotenv'
 import { Post, Feed } from './models'
@@ -9,14 +8,11 @@ import graphqlRouter from './graphql'
 const app = express();
 
 const { PORT = 8080 } = process.env;
-app.set('views', path.join(__dirname, './views'))
-// app.set('view engine', 'twig');
-app.set('view engine', 'html');
-app.engine('html', twig.__express);
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
 app.set('cache', false);
 
 app.use(graphqlRouter);
-
 
 app.get('/', async (req, res) => {
     const posts = await Post.find({})
@@ -31,6 +27,5 @@ app.get('/feeds', async (req, res) => {
         .sort({'date': -1});
     res.render('feeds', { feeds });
 });
-
 
 app.listen(PORT);
