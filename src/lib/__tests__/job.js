06932ce6bdd2de4edc02f22f2242a5156a3323b0
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2018-04-02 14:09:40 -0300

    Testing response handler in job.crawlFeed

--- src/lib/__tests__/job.js
+++ src/lib/__tests__/job.js
@@ -2,11 +2,17 @@
 
 // mocking
 jest.mock('request');
+jest.mock('feedparser');
 import requestMock from 'request';
+import fpMock from 'feedparser';
 
 import { crawlFeed } from '../job'
 
 describe('crawlFeed', () => {
+  beforeEach(() => {
+    fpMock.mockClear();
+  });
+
   it('assert request calls', () => {
     const req = { on: jest.fn() };
     req.on.mockReturnValue(req);
@@ -15,7 +21,7 @@ describe('crawlFeed', () => {
     const url = `some.url/${Math.random()}`;
     crawlFeed(url);
 
-    expect(requestMock).toHaveBeenCalled();
+    expect(requestMock).toHaveBeenCalledTimes(1);
     expect(requestMock).toHaveBeenCalledWith(url);
 
     expect(req.on.mock.calls).toMatchSnapshot();
@@ -23,7 +29,7 @@ describe('crawlFeed', () => {
 
   it('assert error handler', () => {
     const req = { on: jest.fn() };
-    req.on.mockReturnValue(req);
+    req.on.mockReturnThis();
     requestMock.mockReturnValue(req);
 
     const callback = jest.fn();
@@ -34,7 +40,28 @@ describe('crawlFeed', () => {
     const handler = req.on.mock.calls.filter(call => call[0] === 'error')[0][1];
     handler(forcedError);
 
-    expect(callback).toHaveBeenCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
     expect(callback).toHaveBeenCalledWith(forcedError);
+  });
+
+  it('assert response handler', () => {
+    const fp = { on: jest.fn() };
+    const req = { on: jest.fn(), pipe: jest.fn() };
+    fp.on.mockReturnThis();
+    req.on.mockReturnThis();
+    requestMock.mockReturnValue(req);
+    fpMock.mockImplementation(() => fp);
+
+    const callback = jest.fn();
+    const url = `some.url/${Math.random()}`;
+    crawlFeed(url, callback);
+
+    const handler = req.on.mock.calls.filter(call => call[0] === 'response')[0][1];
+    handler.bind(req)();
+
+    expect(req.pipe).toHaveBeenCalledTimes(1);
+    expect(req.pipe).toHaveBeenCalledWith(fp);
+
+    expect(fp.on.mock.calls).toMatchSnapshot();
   });
 });
